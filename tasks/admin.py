fd81f53cfaf31830f95fcb46165c3f3511347140
JOSUE REYES <antoniojosuerp@ufps.edu.co>
2024-05-30 15:15:29 -0500

    feat: Add search functionality to TaskAdmin

--- tasks/admin.py
+++ tasks/admin.py
@@ -5,3 +5,4 @@ from .models import Task
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'description',  'priority', 'user', 'completed',]
+    search_fields = ['name', 'description', 'user__username']
