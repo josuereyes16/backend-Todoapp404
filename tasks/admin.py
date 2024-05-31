d0ffbae0108602975ce3dc850f24200b530deccc
Angelo Salamanca <angelosalamanca21@gmail.com>
2024-05-31 11:01:20 -0500

    feat: Update admin profiles and tasks, and models tasks

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,8 +1,15 @@
-
 from django.contrib import admin
 from .models import Task
 
+
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'description',  'priority', 'user', 'completed',]
-    search_fields = ['name', 'description', 'user__username']
+    list_display = [
+        "id",
+        "name",
+        "description",
+        "priority",
+        "user",
+        "completed",
+    ]
+    search_fields = ["name"]
