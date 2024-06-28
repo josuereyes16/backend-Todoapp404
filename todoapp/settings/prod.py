ba1fcc77cf10677e74695082c5ef61145014247c
JOSUE REYES <antoniojosuerp@ufps.edu.co>
2024-06-28 00:29:51 -0500

    feat: update requirements.txt

--- todoapp/settings/prod.py
+++ todoapp/settings/prod.py
@@ -1,7 +1,6 @@
-from .base import *
+from .base import some_setting, another_setting  # noqa: F401
 
 from decouple import config
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -9,20 +8,17 @@ from decouple import config
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG")
+DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOSTS").split()
-
+ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOSTS", default="").split()
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-
 DATABASES = {
     "default": {
-        "ENGINE": config("DB_ENGINE"),
+        "ENGINE": config("DB_ENGINE", default="django.db.backends.postgresql"),
         "NAME": config("DB_DATABASE"),
         "USER": config("DB_USER"),
         "PASSWORD": config("DB_PASSWORD"),
