c313af48f24e353c6c19947efcb6cc823eb3e1ac
JOSUE REYES <antoniojosuerp@ufps.edu.co>
2024-06-28 01:13:42 -0500

    chore: Update SECRET_KEY default value in prod.py

--- todoapp/settings/prod.py
+++ todoapp/settings/prod.py
@@ -7,7 +7,8 @@ from decouple import config
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY", default="your_default_secret_key")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
